1667df7b2f1d09ad2346e6d1ad5b31b0f74b8527
OmgbaKevin <kevinomgba1@gmail.com>
2025-11-05 00:27:35 +0100

    adjustes settings.py

--- voteproject/settings.py
+++ voteproject/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -132,3 +133,8 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 ELECTION_WINDOW_SECONDS = 300
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.pythonanywhere.com",
+    "https://ton-frontend.vercel.app",  # remplace par ton domaine réel de front
+]
